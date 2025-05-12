4dc4c334d8acc03604fd7a42bd43114bb2910f7e
David Kaplansky <dkaplanskybrimmer@gmail.com>
2025-05-12 22:27:38 +0200

    Initial Commit

---
+++ MyThoughtfulDiary/MyThoughtfulDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyThoughtfulDiaryApp.swift
+//  MyThoughtfulDiary
+//
+//  Created by David Kaplansky on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MyThoughtfulDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
